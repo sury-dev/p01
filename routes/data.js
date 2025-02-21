9dd3214aaad49f4a2ac2868ddd6de179d67f41c3
Suryansh Pandey <coderhood0011@gmail.com>
2025-02-21 17:56:35 +0530

    Commit 2

--- routes/data.js
+++ routes/data.js
@@ -2,7 +2,10 @@ const express = require("express");
 const {postData, getData}=require("../controller/dataController")
 
 const dataRouter=express.Router();
-
+// return OK message
+dataRouter.route('/').get((req, res) => {
+    res.send('OK');
+  });
 dataRouter.route('/bfhl').get(getData).post(postData);
 
 module.exports=dataRouter;
